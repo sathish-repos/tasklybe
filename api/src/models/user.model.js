514a1a587a087507dc0973904cc8be177b380d96
sathish <me.sathish.git@gmail.com>
2025-06-13 21:04:10 +0530

    create user model

---
+++ api/src/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+      trim: true,
+      minLength: 3,
+      maxLength: 25,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      trim: true,
+      lowerCase: true,
+      validate: {
+        validator: function (value) {
+          return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
+        },
+        message: (props) => `${props.value} is not a valid email ID`,
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      minLength: 6,
+      maxLength: 12,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
