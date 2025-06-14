68db366f5ed0f2a1a4a30f87b0d11181e51b36ce
sathish <me.sathish.git@gmail.com>
2025-06-14 17:51:53 +0530

    implement task model and enhance task controller with authorization

--- api/src/controllers/task.controller.js
+++ api/src/controllers/task.controller.js
@@ -1,8 +1,29 @@
 import sendResponse from "../middlewares/response.middleware.js";
+import Task from "../models/task.model.js";
+
+export const createTask = async (req, res, next) => {
+  try {
+    // const dueDate = "2025-06-15T12:02:07.878Z";
+    const task = await Task.create({
+      ...req.body,
+      userId: req.user._id,
+    });
+    sendResponse(res, 201, "task created successfully!", { task });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getTask = async (req, res, next) => {
   try {
-    let task = {};
+    let task = await Task.findById(req.params.id);
+
+    if (!task)
+      sendResponse(res, 404, `Task with ID: ${req.params.id} is not found!`);
+
+    if (task.userId.toString() !== req.user._id.toString())
+      sendResponse(res, 401, "Unauthorized user");
+
     sendResponse(res, 200, `Got the task with ID: ${req.params.id}`, { task });
   } catch (error) {
     next(error);
@@ -11,7 +32,11 @@ export const getTask = async (req, res, next) => {
 
 export const getUserTasks = async (req, res, next) => {
   try {
-    let tasks = [];
+    if (req.user._id.toString() !== req.params.id)
+      sendResponse(res, 401, "Unauthorized user");
+
+    const tasks = await Task.find();
+
     sendResponse(res, 200, `Got all tasks with user ID: ${req.params.id}`, {
       tasks,
     });
--- api/src/routes/task.routes.js
+++ api/src/routes/task.routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import { getTask, getUserTasks } from "../controllers/task.controller.js";
+import {
+  createTask,
+  getTask,
+  getUserTasks,
+} from "../controllers/task.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const taskRouter = Router();
 
-taskRouter.get("/:id", getTask);
+taskRouter.get("/:id", authorize, getTask);
 
-taskRouter.get("/user/:id", getUserTasks);
+taskRouter.get("/user/:id", authorize, getUserTasks);
+
+taskRouter.post("/", authorize, createTask);
 
 export default taskRouter;
