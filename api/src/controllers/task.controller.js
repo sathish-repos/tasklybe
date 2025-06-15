3209a9edc7fffbc628b8393564fb3efcd8b58c9e
sathish <me.sathish.git@gmail.com>
2025-06-15 10:57:57 +0530

    implement task update and delete functionalities with authorization checks

--- api/src/controllers/task.controller.js
+++ api/src/controllers/task.controller.js
@@ -32,14 +32,53 @@ export const getTask = async (req, res, next) => {
 
 export const getUserTasks = async (req, res, next) => {
   try {
+    const userId = req.params.id;
     if (req.user._id.toString() !== req.params.id)
       sendResponse(res, 401, "Unauthorized user");
 
-    const tasks = await Task.find();
+    const tasks = await Task.find({ userId });
 
     sendResponse(res, 200, `Got all tasks with user ID: ${req.params.id}`, {
       tasks,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateTask = async (req, res, next) => {
+  try {
+    if (req.user._id.toString() !== req.body.userId)
+      sendResponse(res, 401, "Unauthorized user");
+
+    const taskId = req.params.id;
+    const updatedTask = req.body;
+
+    const result = await Task.findByIdAndUpdate(taskId, updatedTask, {
+      new: true,
+    });
+
+    if (!result) return sendResponse(res, 400, "Updating unsuccessful!");
+
+    sendResponse(res, 201, "Updated successfully!", { result }, true);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteTask = async (req, res, next) => {
+  try {
+    const taskId = req.params.id;
+    const task = await Task.findById(taskId);
+
+    if (!task) return sendResponse(res, 404, "Task not found!");
+
+    if (req.user._id.toString() !== task.userId.toString())
+      sendResponse(res, 401, "Unauthorized user");
+
+    await Task.findByIdAndDelete(taskId);
+
+    sendResponse(res, 200, "Deleted successfully!", null, true);
   } catch (error) {
     next(error);
   }
--- api/src/routes/task.routes.js
+++ api/src/routes/task.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import {
   createTask,
+  deleteTask,
   getTask,
   getUserTasks,
+  updateTask,
 } from "../controllers/task.controller.js";
 import authorize from "../middlewares/auth.middleware.js";
 
@@ -13,5 +15,9 @@ taskRouter.get("/:id", authorize, getTask);
 taskRouter.get("/user/:id", authorize, getUserTasks);
 
 taskRouter.post("/", authorize, createTask);
+
+taskRouter.patch("/:id", authorize, updateTask);
+
+taskRouter.delete("/delete/:id", authorize, deleteTask);
 
 export default taskRouter;
